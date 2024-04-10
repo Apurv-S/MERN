0fec0026dd2934784348ca34e916dccc9712656a
Apoorva Saraswat <102540923+Apurv-S@users.noreply.github.com>
2024-04-10 06:14:01 +0530

    Create script.js

---
+++ DAY 6/NodeJS/script.js
@@ -0,0 +1,12 @@
+const obj = require('./module1');
+obj.sum(20, 5);
+obj.sub(15, 2);
+console.log(obj.name);
+
+// obj.fn2(10,2);
+// console.log("\n : sum:",sum)
+
+// object restructuring
+
+sum(2,4);
+console.log(name);
